19a567cac347e5c07e6c5cfe230e72fff23ae37f
dafeige <364583707@qq.com>
2019-04-28 20:32:24 +0800

    center commit

---
+++ eureka-project/src/main/java/com/study/register/center/eureka/eurekaproject/EurekaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.register.center.eureka.eurekaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProjectApplication.class, args);
+    }
+
+}
